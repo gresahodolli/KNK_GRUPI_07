53c6fb46b1cf5316fa262ab5dde243023f8291b0
lirahoxha <94935407+lirahoxha@users.noreply.github.com>
2022-04-12 12:51:09 +0200

    Database

--- Database_KNK/tables.sql
+++ Database_KNK/tables.sql
@@ -26,3 +26,23 @@ CREATE TABLE `sistemiautobuseve`.`users` (
     REFERENCES `sistemiautobuseve`.`roles` (`id`)
     ON DELETE SET NULL
     ON UPDATE NO ACTION);
+    
+    CREATE TABLE `sistemiautobuseve`.`timetable` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `time` TIME NOT NULL,
+  PRIMARY KEY (`id`));
+
+
+CREATE TABLE `sistemiautobuseve`.`companies` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `name` VARCHAR(45) NOT NULL,
+  `no_buses` INT NULL,
+  `manager_id` INT NULL,
+  PRIMARY KEY (`id`),
+  INDEX `manager_id_idx` (`manager_id` ASC),
+  CONSTRAINT `manager_id`
+    FOREIGN KEY (`manager_id`)
+    REFERENCES `sistemiautobuseve`.`users` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION);
+    
